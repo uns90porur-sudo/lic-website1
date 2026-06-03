888d5aeebfbb13751199a9f9871d738e9625ed0b
uns90porur-sudo <uns90porur-sudo@users.noreply.github.com>
2026-06-03 17:49:14 +0530

    Remove redundant 'LIC' from agent code globally

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Global Configuration for the LIC Agent Website
 const AGENT_CONFIG = {
     name: "R. Neelakandan",
-    agentCode: "LIC0213471W",
+    agentCode: "0213471W",
     phone: "+91 9444040525",
     phoneFormatted: "+91 94440 40525",
     whatsappNumber: "919444040525"
